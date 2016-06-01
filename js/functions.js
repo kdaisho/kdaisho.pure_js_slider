7288753c5143b4c5ab31f3b81ab152863a8e49fa
Daisho <daishokomiyama@gmail.com>
2016-06-01 19:23:57 -0400

    First commit of refactoring

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,11 @@
   var imagecount = 1,
       total = 3,
       image = document.getElementById('container'),
+
+      slide_1 = document.getElementById('slide_1'),
+      slide_2 = document.getElementById('slide_2'),
+      slide_3 = document.getElementById('slide_3'),
+
       nav = document.getElementById('nav'),
       first = document.getElementById('first'),
       second = document.getElementById('second'),
@@ -31,17 +36,30 @@
   // }
 
   first.onclick = function() {
-    image.className = "slide_1";
+    // image.className = "slide_1";
+    // imagecount = 1;
+    slide_1.className = "display";
+    slide_2.className = "";
+    slide_3.className = "";
     imagecount = 1;
     pager1();
   }
   second.onclick = function() {
-    image.className = "slide_2";
+    // image.className = "slide_2";
+    // imagecount = 2;
+    // pager2();
+    slide_1.className = "";
+    slide_2.className = "display";
+    slide_3.className = "";
     imagecount = 2;
     pager2();
   }
   third.onclick = function() {
-    image.className = "slide_3";
+    // image.className = "slide_3";
+    // imagecount = 3;
+    slide_1.className = "";
+    slide_2.className = "";
+    slide_3.className = "display";
     imagecount = 3;
     pager3();
   }
@@ -54,21 +72,30 @@
   transition = function() {
     imagecount = imagecount + 1;
 
-    if(imagecount > total){
+    if (imagecount > total){
       imagecount = 1;
     }
-    else if(imagecount < 1){
+    else if(imagecount =< 0){
       imagecount = total;
     }
 
-    image.className = "slide_" + imagecount;
-    if(image.className == "slide_1") {
+    image.id = "slide_" + imagecount;
+    // if (image.className == "slide_1") {
+    //   pager1();
+    // }
+    // else if (image.className == "slide_2") {
+    //   pager2();
+    // }
+    // else if (image.className == "slide_3") {
+    //   pager3();
+    // }
+    if (slide_1.className == "display") {
       pager1();
     }
-    else if(image.className == "slide_2") {
+    else if (slide_2.className == "slide_2") {
       pager2();
     }
-    else if(image.className == "slide_3") {
+    else if (slide_3.className == "slide_3") {
       pager3();
     }
   };
@@ -79,7 +106,7 @@
     second.className = "";
     third.className = "";
     first.className = "active";
-    text1.className = "show";
+    text1.className = "display";
     text2.className = "";
     text3.className = "";
   }
@@ -87,7 +114,7 @@
     first.className = "";
     third.className = "";
     second.className = "active";
-    text2.className = "show";
+    text2.className = "display";
     text1.className = "";
     text3.className = "";
   }
@@ -95,7 +122,7 @@
     first.className = "";
     second.className = "";
     third.className = "active";
-    text3.className = "show";
+    text3.className = "display";
     text1.className = "";
     text2.className = "";
   }
